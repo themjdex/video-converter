adcd2a83982b41e50e0dfb72ecef23222b9b1d82
Marat Zhanpeisov <themjdex@gmail.com>
2022-04-29 11:24:54 +0500

    first commit

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+class App {
+    run() {
+        console.log('Done');
+    }
+}
+exports.App = App;
+const app = new App();
+app.run();
